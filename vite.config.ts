cb229ac7d201bc1187880da2262d4de3044990e6
Sacramentix <benji.u1225u@gmail.com>
2022-05-03 10:48:22 +0200

    pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        sourcemap: true,
+        cleanupOutdatedCaches: false  
+      },
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],  
+      manifest: {
+        name: 'Frechi',
+        short_name: 'Frechi',
+        description: 'Frechi le jeu des chiffre',
+        theme_color: '#222222',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'favicon.svg',
+            sizes: 'any',
+            type: "image/svg+xml",
+            purpose: 'any maskable',
+          }
+        ]
+      }
+    })
+  ]
 })
