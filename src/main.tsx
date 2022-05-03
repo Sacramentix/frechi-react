7ee942c3bc340959956ca0af32c32484a888112d
Sacramentix <benji.u1225u@gmail.com>
2022-05-03 11:21:53 +0200

    register the service worker

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { GlobalProvider } from './store/game'
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onOfflineReady() {},
+})
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
